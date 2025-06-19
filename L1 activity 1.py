bceb1a84c5d441b1aa4627b461f71ae4c9deb14a
yushra0 <zayaanyushra@gmail.com>
2025-06-19 20:39:41 +0600

    Add files via upload

---
+++ L1 activity 1.py
@@ -0,0 +1,11 @@
+# printing a great message
+print("Hello, world!")
+
+# printing my name
+print("My name is Yushra.")
+
+# printing a fun fact about myself
+print("I love doing coding.")
+
+# performing and printing a simple calculation
+print("The result of 3+5 is:", 3 + 5)
